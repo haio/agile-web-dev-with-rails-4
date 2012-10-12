d6538c4baddf10865b651b56db2241f9b0e41ca1
johny <mockingror@gmail.com>
2012-10-13 01:12:25 +0800

    integration test

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'execjs'
 
 gem 'therubyracer'
 
+gem 'will_paginate', '>= 3.0.pre'
+
 # To use ActiveModel has_secure_password
 
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,4 +34,17 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Config Gmail
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => "domain.of.sender.net",
+    :authentication => "plain",
+    :user_name => "zhongyu.johny@gmail.com",
+    :password => "mockingDuck910204",
+    :enable_starttls_auto => true
+  }
 end
